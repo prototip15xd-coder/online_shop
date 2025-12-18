b5b31468e68e6e809e3fbcec7f7eff5a066d4a1b
Marina R <prototip15xd@gmail.com>
2025-12-18 17:40:16 +0700

    сделала возможность редакировать данные профиля

--- src/public/index.php
+++ src/public/index.php
@@ -1,83 +1,19 @@
-<form action="registration_page.php">
-    <div class="container">
-        <h1>Register</h1>
-        <p>Please fill in this form to create an account.</p>
-        <hr>
+<?php
 
-        <label for="name"><b>Name</b></label>
-        <input type="text" placeholder="Enter name" name="name" id="name" required>
+$requestUri = $_SERVER['REQUEST_URI'];
+$requestMethod = $_SERVER['REQUEST_METHOD'];
 
-        <label for="email"><b>Email</b></label>
-        <input type="text" placeholder="Enter Email" name="email" id="email" required>
-
-        <label for="psw"><b>Password</b></label>
-        <input type="password" placeholder="Enter Password" name="psw" id="psw" required>
-
-        <label for="psw-repeat"><b>Repeat Password</b></label>
-        <input type="password" placeholder="Repeat Password" name="psw-repeat" id="psw-repeat" required>
-        <hr>
-
-        <p>By creating an account you agree to our <a href="#">Terms & Privacy</a>.</p>
-        <button type="submit" class="registerbtn">Register</button>
-    </div>
-
-    <div class="container signin">
-        <p>Already have an account? <a href="#">Sign in</a>.</p>
-    </div>
-</form>
-<style>
-    * {box-sizing: border-box}
-
-    /* Add padding to containers */
-    .container {
-        padding: 16px;
+if ($requestUri ==='/registration') {
+    if ($requestMethod === 'POST') {   //помоему надо наоборот пост и гет написать
+        require_once './registration_form.php';
+    } elseif ($requestMethod === 'GET') {
+        require_once './handle_login.php';
     }
-
-    /* Full-width input fields */
-    input[type=text], input[type=password] {
-        width: 100%;
-        padding: 15px;
-        margin: 5px 0 22px 0;
-        display: inline-block;
-        border: none;
-        background: #f1f1f1;
-    }
-
-    input[type=text]:focus, input[type=password]:focus {
-        background-color: #ddd;
-        outline: none;
-    }
-
-    /* Overwrite default styles of hr */
-    hr {
-        border: 1px solid #f1f1f1;
-        margin-bottom: 25px;
-    }
-
-    /* Set a style for the submit/register button */
-    .registerbtn {
-        background-color: #04AA6D;
-        color: white;
-        padding: 16px 20px;
-        margin: 8px 0;
-        border: none;
-        cursor: pointer;
-        width: 100%;
-        opacity: 0.9;
-    }
-
-    .registerbtn:hover {
-        opacity:1;
-    }
-
-    /* Add a blue text color to links */
-    a {
-        color: dodgerblue;
-    }
-
-    /* Set a grey background color and center the text of the "sign in" section */
-    .signin {
-        background-color: #f1f1f1;
-        text-align: center;
-    }
-</style>
+    require_once './registration_form.php';
+} elseif ($requestUri ==='/login') {
+    require_once './login_page.php';
+} elseif ($requestUri ==='/handle_login') {
+    require_once './handle_login.php';
+} else {
+    echo '404 Not Found';  //добавить стиль и файлик для нее
+}
