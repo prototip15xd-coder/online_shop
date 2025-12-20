b2c35b9166cdc6a059e7f95ab70f8619f28cbb89
Marina R <prototip15xd@gmail.com>
2025-12-20 13:23:12 +0700

    сделала возможность редакировать данные профиля

--- src/public/index.php
+++ src/public/index.php
@@ -4,16 +4,20 @@ $requestUri = $_SERVER['REQUEST_URI'];
 $requestMethod = $_SERVER['REQUEST_METHOD'];
 
 if ($requestUri ==='/registration') {
-    if ($requestMethod === 'POST') {   //помоему надо наоборот пост и гет написать
+    if ($requestMethod === 'GET') {   //помоему надо наоборот пост и гет написать
         require_once './registration_form.php';
-    } elseif ($requestMethod === 'GET') {
+    } elseif ($requestMethod === 'POST') {
         require_once './handle_login.php';
+    } else {
+        echo "$requestMethod для адреса $requestUri не поддерживается";
     }
+
     require_once './registration_form.php';
 } elseif ($requestUri ==='/login') {
     require_once './login_page.php';
 } elseif ($requestUri ==='/handle_login') {
     require_once './handle_login.php';
 } else {
-    echo '404 Not Found';  //добавить стиль и файлик для нее
+    http_response_code(404);
+    require_once './404.php';
 }
